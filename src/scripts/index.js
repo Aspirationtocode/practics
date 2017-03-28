98e60305c237df23b8f8c99ee641633b3738d0ca
aspirationtocode <aspiretocode@gmail.com>
2017-03-28 13:29:21 +0300

    add loading images and fonts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,4 @@ const css = require('../styles/main.styl');
 
 import math from './modules/math';
 
-console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, 3)} times!`);
+console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, 3 ** 3)} times!`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,60 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const dist = path.resolve('dist');
-const source = path.resolve('src/scripts/index.js');
+const dist = __dirname + '/dist';
 
 module.exports = {
-  entry: source,
-  output: {
-    path: dist,
-    filename: 'app.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.styl$/,
-        exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'stylus-loader'],
-          publicPath: dist
-        })
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.pug$/,
-        use: ["pug-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|ttf)$/,
-        loader: 'url-loader'
-      }
-    ]
-  },
-  devServer: {
-    contentBase: dist,
-    compress: true,
-    port: 3000,
-    stats: 'errors-only',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Project',
-      template: path.resolve('src/views/index.pug'),
-      hash: true
-    }),
-    new ExtractTextPlugin({
-      filename: 'app.css',
-      disable: false,
-      allChunks: true
-    })
-  ]
+	context: __dirname + '/src',
+	entry: './scripts/index.js',
+	output: {
+		path: dist,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.styl$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'stylus-loader']
+				})
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: "babel-loader"
+			},
+			{
+				test: /\.pug$/,
+				use: ["pug-loader"]
+			},
+			{
+				test: /\.(jpg)$/,
+				use: "file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/"
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: 'file-loader?name=[name].[ext]&outputPath=fonts/&publicPath=fonts/'
+			}
+		]
+	},
+	devServer: {
+		contentBase: __dirname + '/dist',
+		compress: true,
+		port: 3000,
+		stats: 'errors-only',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Project',
+			template: path.resolve('src/views/index.pug'),
+			hash: true
+		}),
+		new ExtractTextPlugin({
+			filename: 'bundle.css',
+			disable: false,
+			allChunks: true
+		})
+	]
 }

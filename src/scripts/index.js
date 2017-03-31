e236f6dc00e835e3507b35a34157af7705538bcd
aspirationtocode <aspiretocode@gmail.com>
2017-03-31 18:32:56 +0300

    package.json fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,4 +5,4 @@ import css from '../styles/main.styl';
 
 import math from './modules/math';
 
-console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, 3 ** 3)} times!`);
+console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, Math.pow(3, 3))} times!`);

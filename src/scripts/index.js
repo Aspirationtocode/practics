da05c053c8b1518add45b71c20be682ebec54452
aspirationtocode <aspiretocode@gmail.com>
2017-04-04 00:57:44 +0300

    add mixin to grid-template-areas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,7 @@
-require('../views/index.pug');
-require('../views/about.pug');
-
-import css from '../styles/main.styl';
+import '../views/index.pug';
+import '../views/about.pug';
+import '../styles/main.styl';
 
 import math from './modules/math';
 
-console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, Math.pow(3, 3))} times!`);
+console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, 3 ** 3)} times!`);

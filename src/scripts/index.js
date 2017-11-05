da4336f816ab9c7814ce40e905924325eda656ce
Aspirationtocode <aspiretocode@gmail.com>
2017-11-05 21:13:24 +0300

    initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,31 @@ import '../views/index.pug';
 import '../views/about.pug';
 import '../styles/main.styl';
 
-import math from './modules/math';
+import $ from 'jquery';
+import viewport from 'get-viewport-size';
 
-console.log(`Hello from app.js and webpack-dev-server ${math.sum(1, 3 ** 3)} times!`);
+require('jquery-ui-bundle');
+
+import j from './jquery-constants';
+
+j.headerElement.on('click', function() {
+	const currentTabIndex = $(this).data('tab');
+	j.headerElement.removeClass(j.headerElementActiveClass);
+	$(this).addClass(j.headerElementActiveClass);
+	$('.tabs-content-element').removeClass('tabs-content-element--active');
+	$(`.tabs-content-element--${currentTabIndex}`).addClass('tabs-content-element--active');
+});
+
+$('.header-open-mobile-menu').on('click', function() {
+	$(this).toggleClass('header-open-mobile-menu--close');
+	$('.header-elements').toggleClass('header-elements--mobiled');
+	$('.header-overlay').toggleClass('header-overlay--active');
+});
+
+$(window).on('resize', () => {
+	if (viewport().width > 722) {
+		$('.header-elements').removeClass('header-elements--mobiled');
+		$('.header-overlay').removeClass('header-overlay--active');
+		$('.header-open-mobile-menu').removeClass('header-open-mobile-menu--close');
+	}
+});
---
+++ src/scripts/jquery-constants.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+export default {
+	headerElement: $('.header-elements__element'),
+	headerElementActiveClass: 'header-elements__element--active',
+};

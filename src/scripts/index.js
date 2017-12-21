ffc3647d6060dfaa170131da80ef38e765fca4c2
Aspirationtocode <aspiretocode@gmail.com>
2017-12-21 17:29:20 +0300

    fix bugs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,35 +1,8 @@
 import $ from 'jquery';
 import viewport from 'get-viewport-size';
 import '../views/index.pug';
-import '../views/about.pug';
 import '../styles/main.styl';
 import './static-init';
-
-
-
-const dimensionsArray = []
-
-for (let i = 1; i <= 6; i++) {
-	dimensionsArray.push($(`.tabs-content-element--${i} .tabs-content-data`).height())
-}
-
-function resetHeights(currentTabIndex = 1) {
-	for (let i = 1; i <= 6; i++) {
-		if (i !== currentTabIndex) {
-			$(`.tabs-content-element--${i} .tabs-content-data`).css({
-				height: `0`,
-				overflow: 'hidden'
-			})
-		} else {
-			$(`.tabs-content-element--${i} .tabs-content-data`).css({
-				height: `${dimensionsArray[i - 1]}px`,
-				overflow: 'visible'
-			})
-		}
-	}
-}
-
-resetHeights()
 
 import j from './jquery-constants';
 
@@ -39,7 +12,6 @@ j.headerElement.on('click', function() {
 	$(this).addClass(j.headerElementActiveClass);
 	$('.tabs-content-element').removeClass('tabs-content-element--active');
 	$(`.tabs-content-element--${currentTabIndex}`).addClass('tabs-content-element--active');
-	resetHeights(currentTabIndex)
 });
 
 $('.header-open-mobile-menu').on('click', function() {
@@ -54,11 +26,6 @@ $(window).on('resize', () => {
 		$('.header-overlay').removeClass('header-overlay--active');
 		$('.header-open-mobile-menu').removeClass('header-open-mobile-menu--close');
 	}
-	dimensionsArray = []
-	for (let i = 1; i <= 6; i++) {
-		dimensionsArray.push($(`.tabs-content-element--${i} .tabs-content-data`).height())
-	}
-	console.log(dimensionsArray)
 });
 
 $('.dropdown-menu-header').bind('click', function() {
